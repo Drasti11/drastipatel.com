3f0a04028f97ce4cea291ab0935c07df3961cef0
drasti <92512704+Drasti11@users.noreply.github.com>
2026-07-31 11:27:03 +0100

    Update sketch.js

--- fun/sketch.js
+++ fun/sketch.js
@@ -1,37 +1,66 @@
+// 1. Global variables
 let hairs = [];
 
 const NUM_HAIRS = 250;
 const SEGMENTS = 18;
 const SEGMENT_LENGTH = 8;
 
-function setup(){
 
+// 2. p5 setup
+function setup(){
     createCanvas(windowWidth, windowHeight);
 
     for(let x=80; x<width-80; x+=5){
-
         hairs.push(new Hair(x,120));
     }
-
-    stroke(40);
-    noFill();
 }
 
+
+// 3. p5 draw loop
 function draw(){
 
     background(248,246,242);
 
     for(let hair of hairs){
-
         hair.update();
         hair.display();
+    }
+}
+
+
+// 4. Hair class
+class Hair{
+
+    constructor(x,y){
+
+        this.root=createVector(x,y);
+
+        this.points=[];
+
+        for(let i=0;i<SEGMENTS;i++){
+
+            this.points.push(
+                createVector(
+                    x,
+                    y+i*SEGMENT_LENGTH
+                )
+            );
+
+        }
+    }
+
+
+    update(){
+
+        // physics goes here
 
     }
 
-}
 
-function windowResized(){
+    display(){
 
-    resizeCanvas(windowWidth,windowHeight);
+        // drawing goes here
+
+    }
 
 }
